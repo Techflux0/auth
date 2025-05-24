68b5d42f42c666812d20f06930f3ddc38d377957
Techflux0 <abdulrahmanmossadhacker@gmail.com>
2025-05-24 18:14:32 +0300

    try

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,12 @@ const isStrongPassword = (password) => {
          hasSpecialChars;
 };
 
+const generateOTP = () => {
+  return Math.floor(10000000 + Math.random() * 90000000).toString();
+};
+
+module.exports = generateOTP;
+
 // Register user
 const register = async (req, res) => {
   try {
